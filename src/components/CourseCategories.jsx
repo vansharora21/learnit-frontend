3f3912592ed25468e4e2deed831b04fb7d8283d6
prakashgupta27 <prakashgupta272001@gmail.com>
2025-06-13 17:52:43 +0530

    push code

--- src/components/CourseCategories.jsx
+++ src/components/CourseCategories.jsx
@@ -1,16 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
-import { reverseGenerateSlug } from './CourseCards';
-import { param } from 'framer-motion/client';
 
-const CourseCard = ({ title, description ,data}) => {
+const CourseCard = ({ title, description }) => {
   return (
     <div className="card h-100 border shadow-sm transition-hover">
       <Link 
         to={`/CourseDesc/${title.replace(/\s+/g, '-').toLowerCase()}`} 
         className="text-decoration-none d-flex flex-column h-100"
-        state={data}
       >
         <div className=" p-4 text-center d-flex justify-content-center align-items-center" style={{ height: '140px' }}>
           <img 
@@ -37,16 +34,12 @@ const CourseCard = ({ title, description ,data}) => {
 const CourseCategories = () => {
   // const [courses, setCourses] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [sulgData, setSlugData]= useState([])
-  console.log("here is the slug data")
 
   const params = useParams();
-  console.log("params.courseSlug0-=-=-==-=-=-=:", params.courseSlug);
-
-    let name = params.courseSlug;
+  console.log("params.courseSlug:", params.courseSlug);
 
   const fetchCourses = async () => {
-    console.log("here is the anme of slug:", name)
+    if (!params.courseSlug) return;
 
     try {
 
@@ -56,11 +49,10 @@ const CourseCategories = () => {
 
       let res= name.toLowerCase()
       const response = await axios.get(`https://api.learnitfy.com/api/admin/get/courses?categoryName=${res}`);
-      // const response = await axios.get(`https://api.learnitfy.com/admin/get/courses?categoryName=front end`);
       console.log("-----------response", response)
       setSlugData(response.data.data.coursesList);
 
-    } catch (error) {
+      } catch (error) {
       console.error('Error fetching courses:', error);
     } finally {
       setLoading(false);
@@ -82,12 +74,11 @@ const CourseCategories = () => {
           <p>Loading courses...</p>
         ) : (
           <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4">
-            {sulgData.map((course, index) => (
+            {courses.map((course, index) => (
               <div className="col" key={index}>
                 <CourseCard
                   title={course.courseName}
                   description={course.description}
-                  data={course}
                 />
               </div>
             ))}
